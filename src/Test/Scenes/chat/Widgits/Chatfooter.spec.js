0604ec000c25c873aea846d26da4766117229125
komaldeep <komaldeep1993@gmail.com>
2017-05-28 04:11:15 +0530

    test cases for chat footer

--- src/Test/Scenes/chat/Widgits/Chatfooter.spec.js
+++ src/Test/Scenes/chat/Widgits/Chatfooter.spec.js
@@ -5,11 +5,33 @@ import Chatfooter from './../../../../components/Scenes/Chat/Widgits/Chatfooter'
 
 
 describe('<Chatfooter/>', () => {
-    const defaultProps = { name: 'Steve' };
     const component = mount(<Chatfooter/>);
+
+    beforeEach(() => {
+        component.setState({
+            textFieldValue:"hello man how are you"
+        })
+    });
 
     it('Chatfooter is defined', () => {
         expect(component).to.have.length(1);
+    });
+
+    it(' textfield should exist', () => {
+        expect(component.find('TextField')).to.have.length(1);
+    });
+
+    it('RaisedButton should exist ', () => {
+        expect(component.find('RaisedButton')).to.have.length(1);
+    });
+
+    it('Send button works properly', () => {
+        component.find('RaisedButton').simulate('click');
+        component.props('send_button_submit(component.state(textFieldValue)');
+        component.setState({
+            textFieldValue:""
+        })
+        expect(component.state("textFieldValue")).to.eql('');
     });
 
 });
